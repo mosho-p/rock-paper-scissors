6928c294c79ace7b9091d5db7b6ef13d10192895
Marshall Payne <34942223+mosho-p@users.noreply.github.com>
2023-11-22 16:16:09 -0600

    Implement playRound function
    playRound takes the player's choice of rock/paper/scissors and the
    computer's choice and returns who wins the game.

--- game.js
+++ game.js
@@ -1,12 +1,34 @@
 function getComputerChoice() {
     var randomChoice = Math.random();
     if (randomChoice < 1/3) {
-        return 'rock';
+        return 'Rock';
     }
     else if (randomChoice < 2/3) {
-        return 'paper';
+        return 'Paper';
     }
     else {
-        return 'scissors';
+        return 'Scissors';
+    }
+}
+
+function playRound(playerChoice, computerChoice) {
+    var pc = playerChoice.substr(0, 1).toUpperCase() + playerChoice.substr(1).toLowerCase();
+    var cc = computerChoice;
+    
+    if ((cc === 'Rock' && pc == 'Paper')
+        || (cc === 'Paper' && pc == 'Scissors')
+        || (cc === 'Scissors' && pc == 'Rock')) {
+            return `${pc} beats ${cc}! You win!`
+    }
+    else if ((pc === 'Rock' && cc == 'Paper')
+        || (pc === 'Paper' && cc == 'Scissors')
+        || (pc === 'Scissors' && cc == 'Rock')) {
+            return `${cc} beats ${pc}! You lose :(`
+    }
+    else if (cc === pc) {
+        return `Draw.`
+    }
+    else {
+        return `${playerChoice} is not a valid throw. Please choose Rock, Paper or Scissors.`
     }
 }
